e7653a82b95daea9e3d326ac45b542b6b0b7b0c2
program-friedwater <110648659+program-friedwater@users.noreply.github.com>
2023-01-23 20:22:50 +0900

    クイックフィックスっていいね

--- main.py
+++ main.py
@@ -9,27 +9,31 @@ mitoken = 0
 miaurl = 0
 okay = 0
 vertkt = {"a" : "asdf"}
-def login():      
-    if login == 'Misskey':
-        miauth()
-    else:
-        if login == 'Mastodon':
-            if instances == 0:
-             instances = input("Please enter instances url.")
+class fire:
+    def snssetup():
+        login = input("Please enter SNS name.")
+        instance = input("Please enter instances url.")
+
+    def login():      
+        if login == 'Misskey':
+            miauth()
         else:
-            snssetup()
+            if login == 'Mastodon':
+             instances = input("Please enter instances url.")
+            else:
+                snssetup()
 
-def snssetup():
-    login = input("Please enter SNS name.")
-    instance = input("Please enter instances url.")
 # ここからMisskey機能実装
-def miauth():
-    miauthuuid = uuid.uuid4()
-    miaurl = "https://" + instances + "/" + miauthuuid + "/"
-    response = requests.post(miaurl, json=vertkt)
-    okay = input("ACCESS HERE" + miaurl)
-    response_dict = json.loads(response.text)
-    mktkn = response_dict["i"]
+    def miauth():
+        miauthuuid = uuid.uuid4()
+        miaurl = "https://" + instances + "/" + miauthuuid + "/"
+        response = requests.post(miaurl, json=vertkt)
+        okay = input("ACCESS HERE" + miaurl)
+        response_dict = json.loads(response.text)
+        mktkn = response_dict["i"]
 
-def mipost():
-    login() # 適当に置いた
+    def mipost():
+        login() # 適当に置いた
+
+if __name__ == '__main__':
+    fire.Fire(fire)

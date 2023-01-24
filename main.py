75986294f18b5e6c97cd0d1ca03d67770d8c0baa
program-friedwater <110648659+program-friedwater@users.noreply.github.com>
2023-01-24 17:30:21 +0900

    Fireのところだけ治ってない

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import requests
 import json
-from Fire import Fire
+import fire
 import uuid
+
 login = 0
 instances = 0
 miauthuuid = 0
@@ -10,10 +11,6 @@ miaurl = 0
 okay = 0
 vertkt = {"a" : "asdf"}
 class fire:
-    def snssetup():
-        login = input("Please enter SNS name.")
-        instance = input("Please enter instances url.")
-
     def login():      
         if login == 'Misskey':
             miauth()
@@ -22,15 +19,23 @@ class fire:
              instances = input("Please enter instances url.")
             else:
                 snssetup()
+    
+        def snssetup():
+            login = input("Please enter SNS name.")
+            instance = input("Please enter instances url.")
+            
+        def miauth():
+            miauthuuid = uuid.uuid4()
+            miaurl = "https://" + instances + "/" + miauthuuid + "/"
+            response = requests.post(miaurl, json=vertkt)
+            okay = input("ACCESS HERE" + miaurl)
+            response_dict = json.loads(response.text)
+            mktkn = response_dict["i"]
+
+
 
 # ここからMisskey機能実装
-    def miauth():
-        miauthuuid = uuid.uuid4()
-        miaurl = "https://" + instances + "/" + miauthuuid + "/"
-        response = requests.post(miaurl, json=vertkt)
-        okay = input("ACCESS HERE" + miaurl)
-        response_dict = json.loads(response.text)
-        mktkn = response_dict["i"]
+
 
     def mipost():
         login() # 適当に置いた
